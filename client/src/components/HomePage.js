5b52119d33d4563e5143777931a03acc1ac28422
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-07-05 08:16:36 +0100

    Add style file

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "/Style.css";
+import mydata from "./../../../server/data.json";
 
 function HomePage() {
   return (
@@ -9,3 +11,22 @@ function HomePage() {
 }
 
 export default HomePage;
+
+// import React , {useState} from "react";
+
+// const Video = () => {
+
+//     return(
+//         <span className="video_container">
+//             <h3>My Video</h3>
+
+//          {/* /* put add  search component */}
+//             {mydata.map( (movie)=>
+//             <VideoDetails movie={movie}/>
+//           )}
+
+//         </span>
+//     )
+// };
+
+// export default Video ;
