e02c3a9d8f7272a7bb294fe9ab6c9d7a8029c63d
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-07-05 19:13:38 +0100

    Image container style and search bar done!

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,51 @@
 import React from "react";
-import "/Style.css";
-import mydata from "./../../../server/data.json";
+import "./Style.css";
+import myData from "./data.json";
 
 function HomePage() {
   return (
-    <div>
+    <div className="root">
       <h1>This is the home page</h1>
+      <input type="text" placeholder="Search ... " />
+      <button>Search</button>
+      <div>
+        <div class="dropdown">
+          <button onclick="myFunction()" class="dropbtn">
+            Select a country :
+          </button>
+          <div id="myDropdown" class="dropdown-content">
+            <input
+              type="text"
+              placeholder="Search.."
+              id="myInput"
+              onkeyup="filterFunction()"
+            />
+            <a href="#about">About</a>
+            <a href="#base">Base</a>
+            <a href="#blog">Blog</a>
+            <a href="#contact">Contact</a>
+            <a href="#custom">Custom</a>
+            <a href="#support">Support</a>
+            <a href="#tools">Tools</a>
+          </div>
+        </div>
+      </div>
+      <div>
+        {myData.map((Product) => {
+          return (
+            <div className="product-container">
+              <img
+                src={Product.image}
+                className="product-img"
+                alt={Product.prod_name}
+              />
+              <div className="product-discribe">{Product.prod_name}</div>
+              <div className="product-discribe">Price : £{Product.price}</div>
+              <button className="add-btn">Add </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
