9cf67ab99e0f5663e50eaa1f337c6c81d5d0c3c8
dona-shehu <shehu.donna@gmail.com>
2022-07-06 23:07:39 +0100

    Deploy DB in heroku

--- server/server.js
+++ server/server.js
@@ -10,13 +10,6 @@ const port = process.env.PORT || 4444;
 
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//     user: 'codeyourfuture',
-//     host: 'localhost',
-//     database: 'hujreh',
-//     password: 'donashehu',
-//     port: 5432
-// }); 
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -24,21 +17,6 @@ const pool = new Pool({
         rejectUnauthorized: false
     } 
 })
-// export DATABASE_URL=postgres://codeyourfuture:donashehu@localhost:5432/hujreh?sslmode=disable
- 
-//connectionString should be encoded as follows:
-//postgres://<USERNAME>:<PASSWORD>@<HOST>:<PORT>/<DB>
-
-// To set env. variable in Linux / macOS  run these comands in terminal
-// export PORT=numb_of_port
-// export DATABASE_URL=postgres://<USERNAME>:<PASSWORD>@<HOST>:<PORT>/<DB>
-// npm run start or npm run dev
-
-//Connection with the DB locally In my case 
-//export PORT=7777 
-//export DATABASE_URL=postgres://codeyourfuture:donashehu@localhost:5432/cyf_hotels?sslmode=disable
-//npm run dev
-
 
 app.get("/inventory", (req, res) => {
     pool.query('SELECT * FROM products')
